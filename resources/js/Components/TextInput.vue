37d21ba8edea179d13ac4d8f6c10807e21435640
Felipe <felipealencar.0014@gmail.com>
2024-06-07 17:50:23 -0300

    estilização da página de login

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -2,19 +2,25 @@
 import { onMounted, ref } from 'vue';
 import { initFlowbite } from 'flowbite';
 
-defineProps(['icon']);
+const props = defineProps([
+    'typeInput',
+    'nameInput',
+    'idInput',
+    'forLabel',
+    'label'
+]);
 
 const model = defineModel({
     type: String,
     required: true,
 });
 
-const input = ref(null);
+// const input = ref(null);
 
 onMounted(() => {
-    if (input.value.hasAttribute('autofocus')) {
-        input.value.focus();
-    }
+    // if (input.value.hasAttribute('autofocus')) {
+    //     input.value.focus();
+    // }
     initFlowbite();
 });
 
@@ -22,13 +28,18 @@ defineExpose({ focus: () => input.value.focus() });
 </script>
 
 <template>
-    <!-- <div {{icon}}></div> -->
-    hsaushu
-    
-    <input
-        class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        v-model="model"
-        ref="input"
-    />
-    
+    <div class="relative z-0 w-full mb-5 group">
+        <input
+            :type="typeInput"
+            :name="nameInput"
+            :id="idInput"
+            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "
+            v-model="model"
+        />
+
+        <label
+            :for="forLabel"
+            class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >{{ label }}</label>
+    </div>
 </template>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -5,7 +5,8 @@ import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
-import EmailIcon from '@/Components/Icons/EmailIcon.vue';
+import LoginButton from '@/Components/Buttons/LoginButton.vue';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue'
 import { Head, Link, useForm } from '@inertiajs/vue3';
 
 defineProps({
@@ -31,67 +32,84 @@ const submit = () => {
 </script>
 
 <template>
-    <GuestLayout>
-        <Head title="Log in" />
+    <Head title="Log in" />
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
+    <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+        {{ status }}
+    </div>
+
+    <div class="h-screen flex">
+        <!-- Logo -->
+        <div class="hidden lg:flex w-full lg:w-1/2 login_img_section
+        justify-around items-center">
+            <div class="bg-black opacity-20 inset-0 z-0"></div>
+
+            <div class="w-full mx-auto px-20 flex-col items-center space-y-6">
+                <ApplicationLogo class="w-[500px] h-[500px]" color="#FFF" />
+            </div>
         </div>
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
+        <!-- Form -->
+        <div class="flex w-full lg:w-1/2 justify-center items-center bg-white space-y-8">
+            <div class="w-full px-8 md:px-32 lg:px-24">
 
-                <TextInput
-                    :icon = EmailIcon
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    placeholder="exemplo@email.com"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
+                <form @submit.prevent="submit" class="bg-white">
+                    <div class="w-60 mx-auto">
+                        <ApplicationLogo class="w-[250px] h-[120px]" color="#2C314E" />
+                    </div>
 
-                <InputError class="mt-2" :message="form.errors.email" />
+                    <div>
+                        <TextInput 
+                            typeInput="email"
+                            nameInput="email"
+                            idInput="email"
+                            forLabel="email"
+                            label="Digite seu e-mail"
+                            v-model="form.email"
+                        />
+
+                        <InputError class="mt-2" :message="form.errors.email" />
+                    </div>
+
+                    <div class="mt-8">
+                        <TextInput
+                            typeInput="password"
+                            nameInput="password"
+                            idInput="password"
+                            forLabel="password"
+                            label="Digite sua senha"
+                            v-model="form.password"
+                        />
+
+                        <InputError class="mt-2" :message="form.errors.password" />
+                    </div>
+
+                    <div class="mt-4">
+                        <LoginButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing" />
+                    </div>
+
+                    <div class="flex justify-between block mt-4">
+                        <label class="flex items-center">
+                            <Checkbox name="remember" v-model:checked="form.remember" />
+                            <span class="ms-2 text-sm text-gray-600">Lembre-se de mim</span>
+                        </label>
+                        
+                        <Link
+                            v-if="canResetPassword"
+                            :href="route('password.request')"
+                            class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hover:-translate-y-1 duration-500 transition-all"
+                        >
+                            Esqueceu sua senha?
+                        </Link>
+                    </div>
+                </form>
             </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ms-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Forgot your password?
-                </Link>
-
-                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </PrimaryButton>
-            </div>
-        </form>
-    </GuestLayout>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+    .login_img_section {
+        background: url(https://images.pexels.com/photos/248159/pexels-photo-248159.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1) center center;
+    }
+</style>
